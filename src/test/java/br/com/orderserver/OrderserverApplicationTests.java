ef39c56755fc28a29cfcc41b989cfe0c0e8934c7
dogaomf <douglasmfcomercial@gmail.com>
2024-12-20 14:14:15 -0300

    commit inicial do projeto

--- src/test/java/br/com/orderserver/OrderserverApplicationTests.java
+++ src/test/java/br/com/orderserver/OrderserverApplicationTests.java
@@ -1,13 +1,7 @@
 package br.com.orderserver;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrderserverApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
